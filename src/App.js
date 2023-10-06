bc519f4ba6722de07b2611a5257ae61b3665670f
vanmuoi24 <domuoigghh@gmail.com>
2023-10-06 10:43:37 +0700

    updata

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-
+import { useEffect, useState } from "react";
 function App() {
-  const [time, settime] = useState(180);
-  useEffect(() => {
-    setInterval(() => {
-      settime((post) => post - 0.5);
-    }, 1000);
-  }, []);
+  const [text, settext] = useState("Loading");
+  const [users, settodos] = useState([]);
 
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(`https://648478f7ee799e321626ba3d.mockapi.io/api/v1/users`)
+        .then((data) => data.json())
+        .then((post) => {
+          settodos(post);
+        });
+      settext("");
+    }, 5000);
+  }, []);
+  const handle = () => {
+    fetch(`https://648478f7ee799e321626ba3d.mockapi.io/api/v1/product`)
+      .then((res) => res.json())
+      .then((data) => {
+        settodos(data);
+      });
+  };
   return (
     <>
-      <button>bấm giờ</button>
-      <h1>{time}</h1>
+      <button onClick={handle}>update</button>
+      <table>
+        <thead>
+          <tr>
+            <td>name</td>
+            <td>email</td>
+            <td>age</td>
+            <td>id</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.age}</td>
+              <td>{user.id}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h1>{text}</h1>
     </>
   );
 }
